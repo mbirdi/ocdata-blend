8b3665371a77c98a57927d42ed658656f76e3d91
matt_lamberti@hotmail.com <matt_lamberti@hotmail.com>
2014-07-04 09:18:08 -0400

    Merge ColumnFeaturesTest5 into ColumnFeaturesTest

--- ocdata_blend/tests/ColumnFeaturesTest.py
+++ ocdata_blend/tests/ColumnFeaturesTest.py
@@ -23,20 +23,18 @@ class ColumnfeaturesTest(unittest.TestCase):
         ##print cF.get_col_names(data)
         self.assertNotEqual(cF.get_col_names(), 0.0)
         
-
-   
-   
     def test_returns_col_names(self):
-    	list_of_list = [('id', 'procurring_entity_id', 'tender_type' , 'tender_registration_number'), ('Brian', 'unladen swallow', 'dead parrot' , '2') ]
-    	data = pd.DataFrame(list_of_list)
+    	data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'], 'year': [2000, 2001, 2002, 2001, 2002],'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
+    	frame = pd.DataFrame(data)
         
         ##  Here the class is instantiated
-        cF = ColFeatures(data)
+        cF = ColFeatures(frame)
         
         actual = cF.get_col_names()
         
+        ##print actual
+        
         ##self.assertNotEqual(cF.get_col_names(), 0.0)
-        expected = ['id', 'procurring_entity_id', 'tender_type' , 'tender_registration_number']
+        expected = ['pop', 'state', 'year']
         
         self.assertListEqual(actual, expected, msg='Lists did not match')
-   
